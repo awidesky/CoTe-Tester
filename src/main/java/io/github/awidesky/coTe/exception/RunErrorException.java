c0e81cb57e090dd2c479ecbaf898e22f5f1a1814
Eugene Hong <awidesky@naver.com>
2025-03-10 09:58:58 +0900

    add possible last input to message

--- src/main/java/io/github/awidesky/coTe/exception/RunErrorException.java
+++ src/main/java/io/github/awidesky/coTe/exception/RunErrorException.java
@@ -2,14 +2,31 @@ package io.github.awidesky.coTe.exception;
 
 public class RunErrorException extends CoTeException {
 
-	private static final long serialVersionUID = 1086909894042655980L;
+	private static final long serialVersionUID = -6694125290473746317L;
 
-	public RunErrorException(int exitcode) {
-		super("Run_Error", "Exit code : " + exitcode);
+	private String lastInput;
+	private int lastInputIndex;
+
+	public RunErrorException(int exitcode, String lastInput, int lastInputIndex) {
+		this("Exit code : " + exitcode, lastInput, lastInputIndex);
 	}
 	
-	public RunErrorException(Throwable cause) {
-		super("Run_Error", cause.toString(), cause);
+	public RunErrorException(Throwable cause, String lastInput, int lastInputIndex) {
+		this(cause.toString(), lastInput, lastInputIndex);
+	}
+	
+	private RunErrorException(String msg, String lastInput, int lastInputIndex) {
+		super("Run_Error", msg + "\nPossible last input(line %d) : \n\"%s\"".formatted(lastInputIndex + 1, lastInput));
+		this.lastInput = lastInput;
+		this.lastInputIndex = lastInputIndex;
 	}
 
+	public String getLastInput() {
+		return lastInput;
+	}
+	
+	public int getLastInputIndex() {
+		return lastInputIndex;
+	}
+	
 }
